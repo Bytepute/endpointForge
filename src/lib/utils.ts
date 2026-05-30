71163b862c42757978beb12dce45aa30352c5d38
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2026-05-31 00:37:33 +0330

    [#14-UPDATE] improved path inputs by having default / when typing path (#14)
    * [UPDATE] improved path inputs by having default / when typing path
    
    * [UPDATE] better error handling with empty path
    
    * [UPDATE] util comment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ *
+ * @param value path input string to add slash at its beginning
+ * @returns path with slash
+ */
+export function ensureLeadingSlash(value: string) {
+  if (!value) return ""
+  if (!value.startsWith("/")) {
+    return `/${value}`
+  }
+
+  return value
+}
