8a431dcd529ba64abf92a0e1f23cdb79c61862e3
amir abaris <thisisamirabaris@gmail.com>
2026-05-30 20:54:00 +0330

    [UPDATE] util comment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+/**
+ *
+ * @param value path input string to add slash at its beginning
+ * @returns path with slash
+ */
 export function ensureLeadingSlash(value: string) {
   if (!value) return ""
   if (!value.startsWith("/")) {
